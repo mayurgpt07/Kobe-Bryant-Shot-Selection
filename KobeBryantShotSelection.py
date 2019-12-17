3f4dca1146f9a20a0fe17fbd91ac54a8e8a43fd6
mayurgpt07 <mayurgpt07@gmail.com>
2019-12-16 22:42:30 -0800

    Visualization Function Added

--- KobeBryantShotSelection.py
+++ KobeBryantShotSelection.py
@@ -25,9 +25,31 @@ def TrainModel(game_date, train_data):
 def prepare_inputs(train_data):
 	oe = OrdinalEncoder()
 	oe.fit(train_data)
+	#print(oe.categories_)
 	train_data_enc = oe.transform(train_data)
 	return train_data_enc
 
+def generateVisual(train_data, inputCol, outputCol):
+	VisualizationDataFrame = pd.DataFrame()
+	inputList = []
+	outputList = []
+	uniqueInputCol = train_data[inputCol].unique()
+	uniqueOutputCol = train_data[outputCol].unique()
+	print(uniqueInputCol, uniqueOutputCol)
+	for i in uniqueInputCol:
+		for j in uniqueOutputCol:
+			inputList.append(str(i) + ' ' + str(j))
+			interValue = np.where((train_data[inputCol] == i) & (train_data[outputCol] == j))
+			outputList.append(np.count_nonzero(interValue))
+	VisualizationDataFrame['X Label'] = pd.Series(inputList)
+	VisualizationDataFrame['Y Label'] = pd.Series(outputList)
+	index = np.arange(len(inputList))
+	plt.bar(index, VisualizationDataFrame['Y Label'])
+	plt.xlabel(inputCol, fontsize=10)
+	plt.ylabel(outputCol, fontsize=10)
+	plt.xticks(index, VisualizationDataFrame['X Label'], fontsize=5, rotation=30)
+	plt.title('Number of Successful or Unsuccessful Shots based on '+inputCol)
+	plt.show()
 
 ShotsMade = pd.read_csv('./data.csv', sep = ',', header = 0)
 print(type(ShotsMade.loc[0,'game_date']))
@@ -38,12 +60,18 @@ ShotsMade['DateTimeinMinutes'] = ShotsMade['game_date'] + " " + "00:" + ShotsMad
 print(ShotsMade['DateTimeinMinutes'])
 ShotsMade['DateinFormat'] = ShotsMade['DateTimeinMinutes'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S').date())
 getPlacesFromLatandLong(ShotsMade)
-print(ShotsMade.head(10))
+#print(ShotsMade.head(10))
 
-print(ShotsMade['shot_made_flag'].unique())
+#print(ShotsMade['shot_made_flag'].unique())
 
 ValidationShotsMade = ShotsMade[pd.isnull(ShotsMade['shot_made_flag'])]
 TrainShotsMade = ShotsMade[pd.notnull(ShotsMade['shot_made_flag'])]
+
+#print("Shot Type Made", TrainShotsMade['shot_type'].unique())
+generateVisual(TrainShotsMade, 'shot_type', 'shot_made_flag')
+generateVisual(TrainShotsMade, 'shot_zone_area', 'shot_made_flag')
+generateVisual(TrainShotsMade, 'combined_shot_type', 'shot_made_flag')
+
 
 CategoricalVariable = ['action_type','combined_shot_type','shot_type','shot_zone_area','shot_zone_basic','shot_zone_range', 'Location', 'opponent','playoffs','period']
 DependentVariable = ['shot_made_flag']
@@ -68,6 +96,5 @@ for i in CategoricalVariable:
 
 #plt.plot(TrainShotsMade['combined_shot_type'], TrainShotsMade['shot_made_flag'], 'o', color='black');
 #plt.hist(ShotsMade['shot_zone_range'], color = 'green')
-
-sns.boxplot(x = 'shot_made_flag', y = 'combined_shot_type', data = TrainShotsMade)
-plt.show()
+#sns.catplot(x = 'shot_made_flag', y = 'combined_shot_type', hue = 'shot_type' ,data = TrainShotsMade)
+#plt.show()
