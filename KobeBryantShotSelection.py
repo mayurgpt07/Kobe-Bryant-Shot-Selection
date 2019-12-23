fa5b23e95d4d8d8d3589922d40304701f7f8cd20
mayurgpt07 <mayurgpt07@gmail.com>
2019-12-23 01:16:10 -0800

    Blank value issue resolved

--- KobeBryantShotSelection.py
+++ KobeBryantShotSelection.py
@@ -11,6 +11,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn import model_selection
 from scipy.sparse import hstack
+from sklearn.decomposition import PCA
 
 #Create Locations From Latitude and Longitude
 def getPlacesFromLatandLong(train_data):
@@ -66,63 +67,73 @@ ShotsMade['DoubleDigitMinutes'] = ShotsMade['minutes_remaining'].apply(lambda x:
 ShotsMade['DoubleDigitSeconds'] = ShotsMade['seconds_remaining'].apply(lambda x: format(x, '02'))
 
 ShotsMade['DateTimeinMinutes'] = ShotsMade['game_date'] + " " + "00:" + ShotsMade['DoubleDigitMinutes'] + ":" + ShotsMade['DoubleDigitSeconds']
-ShotsMade['DateinFormat'] = ShotsMade['DateTimeinMinutes'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S').date())
+ShotsMade['DateinFormat'] = ShotsMade['DateTimeinMinutes'].apply(lambda x: datetime.strptime(x, '%m/%d/%Y %H:%M:%S').date())
 getPlacesFromLatandLong(ShotsMade)
 secondsRemaning(ShotsMade)
 print(ShotsMade['secondsTotal'].max(), ShotsMade['secondsTotal'].min())
 
 ValidationShotsMade = ShotsMade[pd.isnull(ShotsMade['shot_made_flag'])]
 TrainShotsMade = ShotsMade[pd.notnull(ShotsMade['shot_made_flag'])]
-
-
+print('Training Data shape', TrainShotsMade.shape)
 
 # generateVisual(TrainShotsMade, 'shot_type', 'shot_made_flag')
 # generateVisual(TrainShotsMade, 'shot_zone_area', 'shot_made_flag')
 # generateVisual(TrainShotsMade, 'combined_shot_type', 'shot_made_flag')
 # generateVisual(TrainShotsMade, 'shot_zone_basic', 'shot_made_flag')
 # generateVisual(TrainShotsMade, 'period', 'shot_made_flag')
-generateVisual(TrainShotsMade, 'period', 'shot_made_flag')
 
-CategoricalVariable = ['action_type','combined_shot_type','shot_type','shot_zone_area','shot_zone_basic','shot_zone_range', 'Location', 'opponent','playoffs','period']
+CategoricalVariable = ['action_type','combined_shot_type','shot_type','shot_zone_area','shot_zone_basic','shot_zone_range', 'Location', 'opponent','playoffs','period','season']
 DependentVariable = ['shot_made_flag']
-NumericalVariable = ['game_event_id','game_id','lat','loc_x','loc_y','lon','minutes_remaining','season','seconds_remaining','shot_distance']
+NumericalVariable = ['game_event_id','game_id','lat','loc_x','loc_y','lon','minutes_remaining','seconds_remaining','shot_distance','secondsTotal','shot_made_flag']
 
 TrainShotsMadeEncoded = prepare_inputs(TrainShotsMade[CategoricalVariable])
-np.savetxt('FeatureOutput.csv', TrainShotsMadeEncoded, delimiter=',', header = 'action_type,combined_shot_type,shot_type,shot_zone_area,shot_zone_basic,shot_zone_range,Location,opponent,playoffs,period', fmt="%i", comments='')
+print('Train Shots Encoded Size',TrainShotsMadeEncoded.shape)
+np.savetxt('FeatureOutput.csv', TrainShotsMadeEncoded, delimiter=',', header = 'action_type,combined_shot_type,shot_type,shot_zone_area,shot_zone_basic,shot_zone_range,Location,opponent,playoffs,period,season', fmt="%i", comments='')
 
 TrainShotsMadeNumerical = TrainShotsMade[NumericalVariable]
 
 TrainShotsDecoded = pd.read_csv('./FeatureOutput.csv', sep = ',', header = 0)
 
 TrainShotsDecoded = TrainShotsDecoded.astype(int)
+
+print('Train Encoded Read Shape',TrainShotsDecoded.shape)
+
 chi2_features = SelectKBest(chi2, k = 2)
 BestFeatures = chi2_features.fit_transform(TrainShotsDecoded, TrainShotsMade[DependentVariable])
 print(BestFeatures)
+TrainShotsMadeLater = TrainShotsMade[NumericalVariable]
 
 for i in CategoricalVariable:
-	TrainShotsMade[i] = TrainShotsDecoded[i]
+	print(list(TrainShotsMadeLater.columns))
+	tempList = TrainShotsDecoded[i].tolist()
+	print(len(tempList))
+	TrainShotsMadeLater.loc[:,i] = pd.Series(tempList, index = TrainShotsMadeLater.index)
 
-TrainShotsMade = TrainShotsMade.join(pd.get_dummies(TrainShotsMade['shot_type'], prefix = 'shot_type', prefix_sep = '@'))
-TrainShotsMade = TrainShotsMade.join(pd.get_dummies(TrainShotsMade['shot_zone_basic'],  prefix = 'shot_zone_basic', prefix_sep = '@'))
-TrainShotsMade = TrainShotsMade.join(pd.get_dummies(TrainShotsMade['shot_zone_range'],  prefix = 'shot_zone_range', prefix_sep = '@'))
-TrainShotsMade = TrainShotsMade.join(pd.get_dummies(TrainShotsMade['shot_zone_area'],  prefix = 'shot_zone_area', prefix_sep = '@'))
-print(list(TrainShotsMade.columns))
+TrainShotsMadeLater.to_csv('Training.csv', sep = ',', header = True)
+print(TrainShotsMadeLater.shape)
+TrainShotsMadeLater = TrainShotsMadeLater.join(pd.get_dummies(TrainShotsMadeLater['shot_type'], prefix = 'shot_type', prefix_sep = '@'))
+TrainShotsMadeLater = TrainShotsMadeLater.join(pd.get_dummies(TrainShotsMadeLater['combined_shot_type'], prefix = 'combined_shot_type', prefix_sep = '@'))
+TrainShotsMadeLater = TrainShotsMadeLater.join(pd.get_dummies(TrainShotsMadeLater['shot_zone_basic'],  prefix = 'shot_zone_basic', prefix_sep = '@'))
+TrainShotsMadeLater = TrainShotsMadeLater.join(pd.get_dummies(TrainShotsMadeLater['shot_zone_range'],  prefix = 'shot_zone_range', prefix_sep = '@'))
+TrainShotsMadeLater = TrainShotsMadeLater.join(pd.get_dummies(TrainShotsMadeLater['playoffs'], prefix = 'playoffs', prefix_sep = '@'))
+TrainShotsMadeLater = TrainShotsMadeLater.join(pd.get_dummies(TrainShotsMadeLater['period'], prefix = 'period', prefix_sep = '@'))
+print(list(TrainShotsMadeLater.columns))
 
 #plt.plot(TrainShotsMade['combined_shot_type'], TrainShotsMade['shot_made_flag'], 'o', color='black');
 #plt.hist(ShotsMade['shot_zone_range'], color = 'green')
-sns.catplot(x = 'shot_made_flag', y = 'secondsTotal',hue = 'period',data = TrainShotsMade)
-plt.show()
+#sns.catplot(x = 'shot_made_flag', y = 'secondsTotal',hue = 'period',data = TrainShotsMade)
+#plt.show()
 
-IndependentVariables = ['shot_distance','secondsTotal', 'shot_type@0.0', 'shot_type@1.0', 'shot_zone_basic@0.0', 'shot_zone_basic@1.0', 'shot_zone_basic@2.0', 'shot_zone_basic@3.0', 'shot_zone_basic@4.0', 'shot_zone_basic@5.0', 'shot_zone_basic@6.0', 'shot_zone_range@0.0', 'shot_zone_range@1.0', 'shot_zone_range@2.0', 'shot_zone_range@3.0', 'shot_zone_range@4.0']
+IndependentVariables = ['season','shot_distance','secondsTotal','period@0','period@1', 'period@2', 'period@3', 'period@4', 'period@5', 'period@6','combined_shot_type@0', 'combined_shot_type@1', 'combined_shot_type@2', 'combined_shot_type@3', 'combined_shot_type@4', 'combined_shot_type@5', 'shot_zone_basic@0','playoffs@0','playoffs@1','shot_type@0', 'shot_type@1', 'shot_zone_basic@0', 'shot_zone_basic@1', 'shot_zone_basic@2', 'shot_zone_basic@3', 'shot_zone_basic@4', 'shot_zone_basic@5', 'shot_zone_basic@6', 'shot_zone_range@0', 'shot_zone_range@1', 'shot_zone_range@2', 'shot_zone_range@3', 'shot_zone_range@4']
 
-X, y = TrainShotsMade[IndependentVariables], TrainShotsMade[DependentVariable]
+X, y = TrainShotsMadeLater[IndependentVariables], TrainShotsMadeLater[DependentVariable]
 
 X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, y, test_size = 0.33)
 
-LogisticModel = LogisticRegression(penalty = 'elasticnet', C = 0.5, solver = 'saga', max_iter = 4000, l1_ratio = 0.5)
+LogisticModel = LogisticRegression(penalty = 'elasticnet', C = 0.1, solver = 'saga', max_iter = 4000, l1_ratio = 0.5)
 fittedLogisticModel = LogisticModel.fit(X_train, Y_train)
-print(model_selection.cross_val_score(fittedLogisticModel, X_train, Y_train, cv = 10))
+print(model_selection.cross_val_score(fittedLogisticModel, X_train, Y_train, cv = 10), np.mean(model_selection.cross_val_score(fittedLogisticModel, X_train, Y_train, cv = 10)))
 
 SupportVectorModel = SVC(kernel = 'rbf', C = 0.1, cache_size = 10000.0, decision_function_shape = 'ovo')
 FittedSVModel = SupportVectorModel.fit(X_train, Y_train)
-print(model_selection.cross_val_score(FittedSVModel, X_train, Y_train, cv = 3))
+print(model_selection.cross_val_score(FittedSVModel, X_train, Y_train, cv = 3), np.mean(model_selection.cross_val_score(FittedSVModel, X_train, Y_train, cv = 3)))
